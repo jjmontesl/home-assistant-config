f17eba3f78eb7b51961c8f8005fcc171b4dbd9bc
Jose Juan Montes <jjmontes@gmail.com>
2017-02-24 00:18:50 +0100

    Added API.AI callback speech and removed password.

--- ext/apiai/cli/apiai-client.py
+++ ext/apiai/cli/apiai-client.py
@@ -5,12 +5,15 @@ import argparse
 import json
 
 import apiai
+import requests
 
 def main():
 
     parser = argparse.ArgumentParser(description='Client for API.AI.')
     parser.add_argument('text', nargs='*', help="text")
     parser.add_argument('-a', '--access-token', dest='access_token', action='store', type=str, default=None, help='access token')
+    parser.add_argument('-u', '--hass-url', dest='hass_url', action='store', type=str, default=None, help='callback url')
+    parser.add_argument('-p', '--hass-password', dest='hass_password', action='store', type=str, default=None, help='callback password')
 
     args = parser.parse_args()
 
@@ -36,8 +39,22 @@ def main():
 
     if result["status"]["code"] == 200:
 
+        speech = ""
+
         for m in result["result"]["fulfillment"]["messages"]:
+            speech = speech + m['speech'] + "\n\n"
             print(m['speech'])
+
+            if args.hass_url:
+
+                url = '%s/api/services/script/apiai_response' % args.hass_url
+                headers = {'x-ha-access': args.hass_password,
+                           'content-type': 'application/json'}
+                data = { 'notify_text': speech }
+
+                response = requests.post(url, headers=headers, data=json.dumps(data))
+
+                #print("*%s" % response.text)
 
 if __name__ == '__main__':
     main()
